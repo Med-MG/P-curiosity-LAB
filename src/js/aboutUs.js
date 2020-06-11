12259887fde9d1bc2e5cea84925885967e118fa3
Abbassihraf <hraf.abbassi@gmail.com>
2020-06-12 00:47:10 +0100

    About us section DEKSTOP

---
+++ src/js/aboutUs.js
@@ -0,0 +1,8 @@
+const paraInfo = document.querySelector('.aboutUs__para--span')
+const btnMore = document.querySelector('.btnMore')
+
+
+btnMore.addEventListener('click',AbMore)
+function AbMore (){
+    paraInfo.style.display='block';
+}
