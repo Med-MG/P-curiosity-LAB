49d0af6c7fb10e4e2de2705bf327aa291c451a0b
Abbassihraf <hraf.abbassi@gmail.com>
2020-06-12 13:29:29 +0100

    Team section page about us DESKTOP

--- src/js/aboutUs.js
+++ src/js/aboutUs.js
@@ -2,7 +2,9 @@ const paraInfo = document.querySelector('.aboutUs__para--span')
 const btnMore = document.querySelector('.btnMore')
 
 
+
 btnMore.addEventListener('click',AbMore)
 function AbMore (){
     paraInfo.style.display='block';
+    btnMore.style.display='none';
 }
